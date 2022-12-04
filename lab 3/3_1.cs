511eda4ae8d2d8cb389cb8852d5f57834c6c6dd5
Kittipod Lambangchang <tonkittipod@gmail.com>
2022-12-04 23:57:02 +0700

    feat: lab 3

---
+++ lab 3/3_1.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Main
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Kittipod Lambangchang");
+      Console.WriteLine("65010077");
+    }
+  }
+}
---
+++ lab 3/3_2.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Main
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int ID = 0077;
+      Console.WriteLine(ID);
+      Console.WriteLine(ID + 100);
+    }
+  }
+}
